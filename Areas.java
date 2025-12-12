99ce729fa64227139c20226b0c2c68db61f6e7fe
Swapnil Shinde <swapnilshinde9825@gmail.com>
2025-12-12 12:16:00 +0530

    table creation and insert records

--- Areas.java
+++
@@ -1,6 +0,0 @@
-class Areas{
- public static void main(String[] args) {
-    
-
-    
-}}
--- Circle.java
+++
@@ -1,5 +0,0 @@
-public class Circle {
-    public static double area(int rad){
-        return rad*rad*3.142;
-    }
-}
